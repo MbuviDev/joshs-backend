9879a7009b833b5f70ed48eefe0cb04c393436e4
MbuviDev <mbuvi.meshack99@gmail.com>
2024-11-25 17:49:09 +0300

    updates

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,6 +3,8 @@ const multer = require("multer");
 const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 const { protect, admin } = require('../middleware/auth');
 const router = express.Router();
+const Product = require("../models/Product")
+
 
 // Multer setup for file uploads
 const storage = multer.memoryStorage();
@@ -16,5 +18,17 @@ router.get('/:id', getProductById); // Public route: Fetch single product by ID
 router.post('/', protect, admin, upload.single('image'), createProduct); // Add product with image
 router.put('/:id', admin, protect, updateProduct); // Update product
 router.delete('/:id', protect, admin, deleteProduct); // Delete product
+
+// Fetch all products
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find(); // Fetch all products from the database
+    res.json(products); // Return products as JSON
+  } catch (error) {
+    console.error("Error fetching products:", error.message);
+    res.status(500).json({ message: "Failed to fetch products" });
+  }
+});
+
 
 module.exports = router;
